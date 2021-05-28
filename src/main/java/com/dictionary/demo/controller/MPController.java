2cb01f94c92a144c28851bc3133c9c551b389824
jangcwal <jangcwal@ajou.ac.kr>
2021-05-28 22:11:27 +0900

    0528

---
+++ src/main/java/com/dictionary/demo/controller/MPController.java
@@ -0,0 +1,62 @@
+package com.dictionary.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+public class MPController {
+
+    @GetMapping("/MPPage")
+    public String getApi() {
+        StringBuffer result = new StringBuffer();
+        try{
+            String urlStr = "https://open.assembly.go.kr/portal/openapi/npffdutiapkzbfyvr?" +
+                    "Key=fc034b86fe884eb299b8fc089cdc78d4" +
+                    "&Type=json" +
+                    "&pIndex=1" +
+                    "&pSize=1" +
+                    "&MONA_CD=XQ98168F" +
+                    "&UNIT_CD=100020";
+
+            URL url = new URL(urlStr);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"));
+
+            String returnLine = "";
+            while((returnLine = br.readLine()) != null) {
+                result.append(returnLine);
+            }
+            urlConnection.disconnect();
+
+            urlStr = "https://open.assembly.go.kr/portal/openapi/nzmimeepazxkubdpn?" +
+                    "Key=fc034b86fe884eb299b8fc089cdc78d4" +
+                    "&Type=json" +
+                    "&pIndex=1" +
+                    "&pSize=10" +
+                    "&AGE=21" +
+                    "&PROPOSER=%EA%B0%95%EA%B8%B8%EB%B6%80";
+            url = new URL(urlStr);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            br = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"));
+
+            returnLine = "";
+            while((returnLine = br.readLine()) != null) {
+                result.append(returnLine);
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+}
